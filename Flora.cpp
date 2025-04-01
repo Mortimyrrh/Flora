fa47911aa4e383c359b0e884f95c5dcf572d7f2b
Mortimer Pavlitski <MortimerPavlitski@gmail.com>
2025-04-01 23:08:15 +0100

    Add volume dial

--- Flora.cpp
+++ Flora.cpp
@@ -5,23 +5,28 @@ using namespace daisy;
 using namespace patch_sm;
 using namespace daisysp;
 
-DaisyPatchSM hw;
+DaisyPatchSM patch;
+
+float volume = 1;
 
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
-	hw.ProcessAllControls();
+	patch.ProcessAllControls();
+	volume = patch.GetAdcValue(CV_1); // 0 -> 1
+
+
 	for (size_t i = 0; i < size; i++)
 	{
-		OUT_L[i] = IN_L[i];
-		OUT_R[i] = IN_R[i];
+		OUT_L[i] = IN_L[i] * volume;
+		OUT_R[i] = IN_R[i] * volume;
 	}
 }
 
 int main(void)
 {
-	hw.Init();
-	hw.SetAudioBlockSize(4); // number of samples handled per callback
-	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
-	hw.StartAudio(AudioCallback);
+	patch.Init();
+	patch.SetAudioBlockSize(4); // number of samples handled per callback
+	patch.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
+	patch.StartAudio(AudioCallback);
 	while(1) {}
 }
